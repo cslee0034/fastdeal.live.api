cd97d5015df81c10c499d17ec77737fa2243e2bc
cslee0034 <cslee0034@gmail.com>
2024-03-23 20:38:52 +0900

    feat: generateToken 비즈니스 로직 작성

--- src/modules/auth/service/auth.service.spec.ts
+++ src/modules/auth/service/auth.service.spec.ts
@@ -1,12 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { SignUpDto } from '../dto/request/signup.dto';
+import { InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockConfigService = {
+    get: jest.fn((key: string): number | string => {
+      if (typeof key === 'string') {
+        return key;
+      }
+
+      return key;
+    }),
+  };
+
+  interface Payload {
+    userId: number;
+    email: string;
+  }
+  interface SignOption {
+    secret: string;
+    expiresIn: string;
+  }
+  const mockJwtService = {
+    signAsync: jest.fn(
+      (payload: Payload, signOption: SignOption): Promise<string> => {
+        const token = `${payload.userId}_${payload.email}_${signOption.secret}_${signOption.expiresIn}`;
+        return Promise.resolve(token);
+      },
+    ),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +55,73 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('generateToken', () => {
+    const mockSignUpDto: SignUpDto = {
+      email: 'test@email.com',
+      name: 'test_name',
+      password: 'test_password',
+    };
+
+    it('should called with signAsync function', async () => {
+      const id = 1;
+      const email = mockSignUpDto.email;
+      const accessSecret = 'jwt.access.secret';
+      const accessExpiresIn = 'jwt.access.expiresIn';
+      const refreshSecret = 'jwt.refresh.secret';
+      const refreshExpiresIn = 'jwt.refresh.expiresIn';
+
+      await service.generateToken(id, email);
+
+      expect(mockJwtService.signAsync).toBeCalledWith(
+        expect.objectContaining({
+          id: id,
+          email: email,
+        }),
+        expect.objectContaining({
+          secret: accessSecret,
+          expiresIn: accessExpiresIn,
+        }),
+      );
+
+      expect(mockJwtService.signAsync).toBeCalledWith(
+        expect.objectContaining({
+          id: id,
+          email: mockSignUpDto.email,
+        }),
+        expect.objectContaining({
+          secret: refreshSecret,
+          expiresIn: refreshExpiresIn,
+        }),
+      );
+    });
+
+    it('should return token strings object', async () => {
+      const id = 1;
+      const email = mockSignUpDto.email;
+
+      const tokens = await service.generateToken(id, email);
+
+      expect(tokens).toEqual(
+        expect.objectContaining({
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        }),
+      );
+    });
+
+    it('should throw error if it fails to generate token', async () => {
+      const id = 1;
+      const email = 'example@email.com';
+
+      mockJwtService.signAsync.mockRejectedValueOnce(
+        new Error('Failed to create token'),
+      );
+
+      await expect(service.generateToken(id, email)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
   });
 });
--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,3 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Tokens } from '../types/tokens.type';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async generateToken(id: number, email: string): Promise<Tokens> {
+    const payload = {
+      id: id,
+      email: email,
+    };
+
+    try {
+      const [accessToken, refreshToken] = await Promise.all([
+        this.jwtService.signAsync(payload, {
+          secret: this.configService.get<string>('jwt.access.secret'),
+          expiresIn: this.configService.get<string>('jwt.access.expiresIn'),
+        }),
+        this.jwtService.signAsync(payload, {
+          secret: this.configService.get<string>('jwt.refresh.secret'),
+          expiresIn: this.configService.get<string>('jwt.refresh.expiresIn'),
+        }),
+      ]);
+
+      return { accessToken, refreshToken };
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to create token');
+    }
+  }
+}
