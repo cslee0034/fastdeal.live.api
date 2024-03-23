5c0e43e5b30e9842563c9ebe3670ad17e92baefd
cslee0034 <cslee0034@gmail.com>
2024-03-23 22:08:57 +0900

    feat: 환경변수 추가

--- src/config/env/env.ts
+++ src/config/env/env.ts
@@ -9,10 +9,34 @@ export const env = () => ({
     timeout: process.env.HTTP_TIMEOUT,
     max_redirects: process.env.HTTP_MAX_REDIRECTS,
   },
+  rdb: {
+    host: process.env.RDB_HOST,
+    port: +process.env.RDB_PORT,
+    username: process.env.RDB_USERNAME,
+    password: process.env.RDB_PASSWORD,
+    database: process.env.RDB_DATABASE,
+    synchronize: process.env.RDB_SYNCHRONIZE,
+    logging: process.env.RDB_LOGGING,
+    url: process.env.RDB_URL,
+  },
   cache: {
     host: process.env.CACHE_HOST,
     port: process.env.CACHE_PORT,
     password: process.env.CACHE_PASSWORD,
     ttl: process.env.CACHE_TTL,
+  },
+  encrypt: {
+    salt: process.env.ENCRYPT_SALT,
+  },
+  jwt: {
+    access: {
+      secret: process.env.JWT_ACCESS_SECRET,
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+    },
+    refresh: {
+      secret: process.env.JWT_REFRESH_SECRET,
+      expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+      prefix: process.env.JWT_REFRESH_PREFIX,
+    },
   },
 });
