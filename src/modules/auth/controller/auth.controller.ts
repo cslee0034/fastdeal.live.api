8da88d433ef9d01e99ad44ba738771914e2c7cea
cslee0034 <cslee0034@gmail.com>
2024-03-23 20:56:25 +0900

    feat: login controller swagger 작성

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { SignUpDto } from '../dto/request/signup.dto';
 import { UsersService } from '../../users/service/users.service';
 import { AuthService } from '../service/auth.service';
+import { TokensResponseDto } from '../dto/response/token.dto';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +17,10 @@ export class AuthController {
   ) {}
 
   @Post('local/signup')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({ type: TokensResponseDto })
+  @ApiForbiddenResponse()
+  @ApiInternalServerErrorResponse()
   async signup(@Body() signUpDto: SignUpDto) {
     const createdUser = await this.usersService.create(signUpDto);
 
