140149159e0cf442e0e9f42c4f038fdcf0c1f2f4
cslee0034 <cslee0034@gmail.com>
2024-03-23 22:40:34 +0900

    test: 모듈에 AccessTokenStrategy등록

--- src/modules/auth/module/auth.module.spec.ts
+++ src/modules/auth/module/auth.module.spec.ts
@@ -4,6 +4,8 @@ import { AuthController } from '../controller/auth.controller';
 import { AuthService } from '../service/auth.service';
 import { UsersModule } from '../../users/module/users.module';
 import { EncryptModule } from '../../encrypt/module/encrypt.module';
+import { ConfigService } from '@nestjs/config';
+import { AccessTokenStrategy } from '../strategies/access-token.strategy';
 
 describe('AuthModule', () => {
   let authModule: AuthModule;
@@ -12,17 +14,28 @@ describe('AuthModule', () => {
 
   let usersModule: UsersModule;
   let encryptModule: EncryptModule;
+  let accessTokenStrategy: AccessTokenStrategy;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       imports: [AuthModule],
-    }).compile();
+    })
+      .overrideProvider(ConfigService)
+      .useValue({
+        get: jest.fn((key: string) => {
+          if (key === 'jwt.access.secret') {
+            return 'test_access_secret';
+          }
+        }),
+      })
+      .compile();
 
     authModule = module.get<AuthModule>(AuthModule);
     authController = module.get<AuthController>(AuthController);
     authService = module.get<AuthService>(AuthService);
     usersModule = module.get<UsersModule>(UsersModule);
     encryptModule = module.get<EncryptModule>(EncryptModule);
+    accessTokenStrategy = module.get<AccessTokenStrategy>(AccessTokenStrategy);
   });
   it('should be defined', () => {
     expect(authModule).toBeDefined();
@@ -42,5 +55,9 @@ describe('AuthModule', () => {
 
   it('should have encryptModule', () => {
     expect(encryptModule).toBeDefined();
+  });
+
+  it('should have encryptModule', () => {
+    expect(accessTokenStrategy).toBeDefined();
   });
 });
