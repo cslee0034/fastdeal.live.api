d27668f99066c19eb354c2c9b23c38923aafc781
cslee0034 <cslee0034@gmail.com>
2024-03-23 22:38:55 +0900

    feat: AccessTokenStrategy 작성

---
+++ src/modules/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Payload } from '../types/payload.type';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('jwt.access.secret'),
+    });
+  }
+
+  validate(payload: Payload) {
+    return payload;
+  }
+}
