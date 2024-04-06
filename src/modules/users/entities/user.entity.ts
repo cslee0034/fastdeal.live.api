d83df5f82e5ea1c9b0b61515d870e4567c608853
cslee0034 <cslee0034@gmail.com>
2024-04-07 03:24:29 +0900

    chore: 엔티티 부가 설명 추가

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -13,31 +13,49 @@ export enum Role {
 }
 
 export class UserEntity implements User {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The id of the user',
+    example: 0,
+  })
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'test@email.com',
+  })
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ enum: Provider })
   provider: string;
 
   @Exclude() // 인스턴스를 직렬화할 때 해당 필드를 제외
   password: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The first name of the user',
+    example: 'chang su',
+  })
   firstName: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The last name of the user',
+    example: 'lee',
+  })
   lastName: string;
 
-  @ApiProperty()
+  @ApiProperty({ enum: Role })
   role: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The created date of the user',
+    example: '2024-03-01T00:00:00.000Z',
+  })
   createdAt: Date;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The updated date of the user',
+    example: '2024-03-01T00:00:00.000Z',
+  })
   updatedAt: Date;
 
   constructor(partial: Partial<UserEntity>) {
