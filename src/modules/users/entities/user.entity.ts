89107674b668b89356dd4f2555d3be4bce1792c1
cslee0034 <cslee0034@gmail.com>
2024-04-07 01:10:28 +0900

    feat: user entity와 prisma schema 수정
    1. provider 추가
    2. role 추가

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -2,6 +2,16 @@ import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { Exclude } from 'class-transformer';
 
+export enum Provider {
+  LOCAL = 'local',
+  GOOGLE = 'google',
+}
+
+export enum Role {
+  USER = 'user',
+  ADMIN = 'admin',
+}
+
 export class UserEntity implements User {
   @ApiProperty()
   id: number;
@@ -10,10 +20,19 @@ export class UserEntity implements User {
   email: string;
 
   @ApiProperty()
-  name: string;
+  provider: string;
 
   @Exclude() // 인스턴스를 직렬화할 때 해당 필드를 제외
   password: string;
+
+  @ApiProperty()
+  firstName: string;
+
+  @ApiProperty()
+  lastName: string;
+
+  @ApiProperty()
+  role: string;
 
   @ApiProperty()
   createdAt: Date;
