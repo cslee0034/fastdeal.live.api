bd9b13145906e1c5b6f9d096a184960b83b0ba20
cslee0034 <cslee0034@gmail.com>
2024-04-07 01:17:55 +0900

    feat: UsersManager 모듈 추가

--- src/modules/users/module/users.module.ts
+++ src/modules/users/module/users.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from '../service/users.service';
 import { UsersController } from '../controller/users.controller';
-import { UserRepository } from '../repository/users.repository';
+import { UsersRepository } from '../repository/users.repository';
 import { EncryptModule } from '../../encrypt/module/encrypt.module';
 import { PrismaModule } from '../../../common/orm/prisma/module/prisma.module';
+import { UsersManager } from '../manager/users.manager';
 
 @Module({
   imports: [PrismaModule, EncryptModule],
   controllers: [UsersController],
-  providers: [UsersService, UserRepository],
+  providers: [UsersService, UsersRepository, UsersManager],
   exports: [UsersService],
 })
 export class UsersModule {}
