9e9e4b2fc3cc48328efe2c4da6811cb0f3540e95
cslee0034 <cslee0034@gmail.com>
2024-04-21 17:43:20 +0900

    test: app.controller와 app.service 테스트 추가

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let controller: AppController;
+  let appService: AppService;
+
+  const mockService: AppService = {
+    getHello: jest.fn(() => 'Hello World! from nomad-io'),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        {
+          provide: AppService,
+          useValue: mockService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
+  });
+
+  describe('/', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+
+    it('should call getHello from AppService', () => {
+      controller.getHello();
+      expect(appService.getHello).toHaveBeenCalled();
+    });
+
+    it('should return "Hello World!"', () => {
+      expect(controller.getHello()).toBe('Hello World! from nomad-io');
+    });
+  });
+});
