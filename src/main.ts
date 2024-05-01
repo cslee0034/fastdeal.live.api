4f43d419db28d630a7b68d64aa1cbbe4a0eedce1
cslee0034 <cslee0034@gmail.com>
2024-05-01 23:06:17 +0900

    fix: swagger 주소 api 에서 swagger로 변경

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerOptions);
 
   if (environment === 'development') {
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('swagger', app, document);
   }
 
   app.use(json());
