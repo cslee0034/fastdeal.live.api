27dcbc18ba14e6e880dbacbd95450edce7c6af7c
cslee0034 <cslee0034@gmail.com>
2024-06-13 21:23:39 +0900

    fix: city 엔티티 expose, exclude 지정

--- src/modules/cities/entities/city.entity.ts
+++ src/modules/cities/entities/city.entity.ts
@@ -1,68 +1,51 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { City } from '@prisma/client';
+import { Exclude, Expose } from 'class-transformer';
 
 export class CityEntity implements City {
+  constructor(partial: Partial<CityEntity>) {
+    Object.assign(this, partial);
+  }
+
+  @Exclude()
   @ApiProperty({
     description: 'The id of the city',
     example: '1af07b9b-0b1b-4b3d-8e1b-4f4b9b7b1b1b',
   })
   id: string;
 
+  @Expose()
   @ApiProperty({
     description: 'The name of the city',
     example: 'Seoul',
   })
   cityName: string;
 
+  @Exclude()
   @ApiProperty({
     description: 'The country id of the city',
     example: '1af07b9b-0b1b-4b3d-8e1b-4f4b9b7b1b1b',
   })
   countryId: string;
 
-  @ApiProperty({
-    description: 'The weather of the city',
-    example: 'Cloudy',
-  })
-  weather: string;
-
-  @ApiProperty({
-    description: 'The temperature of the city',
-    example: '25',
-  })
-  temperature: number;
-
-  @ApiProperty({
-    description: 'The humidity of the city',
-    example: '50',
-  })
-  humidity: number;
-
-  @ApiProperty({
-    description: 'The wind speed of the city',
-    example: '5',
-  })
-  windSpeed: number;
-
+  @Expose()
   @ApiProperty({
     description: 'The isAvailable status of the city',
     example: 'true',
   })
   isAvailable: boolean;
 
+  @Exclude()
   @ApiProperty({
     description: 'The created date of the city',
     example: '2024-03-01T00:00:00.000Z',
   })
   createdAt: Date;
 
+  @Exclude()
   @ApiProperty({
     description: 'The updated date of the city',
     example: '2024-03-01T00:00:00.000Z',
   })
   updatedAt: Date;
-
-  constructor(partial: Partial<CityEntity>) {
-    Object.assign(this, partial);
-  }
 }
